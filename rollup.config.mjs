6d0abfe41032b8c7ef9f704a682b80b00676f026
Atanas Dimitrov <seishin90@icloud.com>
2024-04-27 17:13:25 +0300

    Cleanup build process

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,8 @@
-import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 import { readFileSync } from 'fs'
-import flatDts from 'rollup-plugin-flat-dts'
 
 const env = process.env.NODE_ENV || 'development'
 
@@ -16,24 +14,16 @@ export default {
     {
       file: pkg.main,
       format: 'esm',
-      sourcemap: true
+      sourcemap: false
     }
   ],
   plugins: [
     replace({ 'process.env.NODE_ENV': JSON.stringify(env), preventAssignment: true }),
     resolve(),
-    babel({
-      babelHelpers: 'runtime',
-      exclude: 'node_modules/**',
-      presets: [['@babel/preset-env', { modules: false }]],
-      plugins: ['@babel/plugin-transform-runtime']
-    }),
     typescript({
-      tsconfig: 'tsconfig.json',
-      declaration: false
+      tsconfig: 'tsconfig.json'
     }),
-    flatDts(),
     terser()
   ],
-  external: ['react', 'react-dom', 'framer-motion']
+  external: ['react', 'react-dom']
 }
